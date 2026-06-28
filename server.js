942e10c27c3a5b403d2967953d2d63837d07129e
Sebastian Iturralde <itusebastian@gmail.com>
2026-06-28 11:17:34 -0500

    Implement routes to serve HTML files for home, organizations, and projects

--- server.js
+++ server.js
@@ -20,11 +20,22 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+  * Routes
+  */
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
 });

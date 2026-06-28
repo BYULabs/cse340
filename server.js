50b1a99efff7b09fcbe6f053ec5fc631d2dcd318
Sebastian Iturralde <itusebastian@gmail.com>
2026-06-28 11:03:24 -0500

    Refactor server.js to include path and fileURLToPath imports for improved file handling

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';

a916d5d045203715b27726c0057bb23a1d5b3921
Sebastian Iturralde <itusebastian@gmail.com>
2026-07-10 20:28:12 -0500

    Enable SSL in development mode for PostgreSQL connection

--- src/models/db.js
+++ src/models/db.js
@@ -15,7 +15,7 @@ const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: process.env.NODE_ENV === 'production' 
         ? { rejectUnauthorized: false } 
-        : false
+        : true
 });
 
 /**
